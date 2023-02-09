5997028e30a0a7b5c3fe4219905d9ad035b1bd64
Gedion Woldeselassie <gwy321@gmail.com>
2023-02-09 14:09:03 -0600

    Update bootstrap.js

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -6,6 +6,7 @@ import App from './App'
 const mount = (el, { onNavigate, defaultHistory, initialPath  } = {}) => {
   const history = defaultHistory || createMemoryHistory({ initialEntries: [initialPath]})
 	onNavigate && history.listen(onNavigate)
+	
   ReactDom.render(
 		<App history={history}/>,
 		el
